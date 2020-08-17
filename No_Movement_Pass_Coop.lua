e29cf6e90f306062072b95fc6f4a949a48d50736
MaeBot <69312348+MaeBot@users.noreply.github.com>
2020-08-17 17:39:40 +0200

    Add files via upload

---
+++ No_Movement_Pass_Coop.lua
@@ -0,0 +1,100 @@
+--Importing TemBotLua
+import ('TemBot.Lua.TemBotLua')
+
+--Registering the Temtem Window
+tblua:RegisterTemTemWindow()
+
+tblua:Sleep(1500)
+
+ while(true)
+ do
+    local RandomSleep = math.random(74, 132)
+    tblua:Sleep(RandomSleep)
+    if tblua:IsInWorld() == true then
+    t = 1
+    end
+            
+    --loop if minimap not detected
+    while tblua:IsInWorld() == false
+    do
+      --if bot is in fight
+      if tblua:IsInFight() == true then
+        --Luma check positive (message and notification)
+        local RandomCheck = math.random(237, 581)
+        tblua:Sleep(RandomCheck)
+        if tblua:CheckLuma() == true then
+          tblua:SendTelegramMessage("Luma Found!")
+          tblua:TestMessage("Luma Found!")
+          tblua:PressKey(0x71)
+        else
+          --Else no Luma, so run away
+          while tblua:IsInWorld() == false
+          do
+            if tblua:IsInFight() == true then
+              local RandomPass = math.random(814, 1294)
+                  tblua:Sleep(RandomPass)
+                  tblua:PressKey(0x36)
+            else
+                    local crx = math.random(1030, 1594)
+                    local crxxx = math.random(1534, 2857)
+                    -- Skill learn
+                    if tblua:GetPixelColor(215, 255) == "0x1CD1D3" then
+                    tblua:Sleep(crxxx)
+                    tblua:PressKey(0x1B)
+                    -- Exp screen F
+                    elseif tblua:GetPixelColor(590, 245) == "0x1CD1D3" then
+                    tblua:Sleep(crx)
+                    tblua:PressKey(0x46)
+                    -- Swap dead tem
+                    elseif tblua:GetPixelColor(1180, 455) == "0x1E1E1E" then
+                    local swap = math.random(4)
+                      if swap == 4 then
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x46)
+                      elseif swap == 3 then
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x46)
+                      elseif swap == 2 then
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x46)
+                      elseif swap == 1 then
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x46)
+                      end
+                    else
+                        tblua:Sleep(crx)
+                    end
+            end
+          end
+        end
+      end
+    end
+  end
